1a3efe66460f27bb803638dfbcae56a876c2920b
Dilshaan <dilshaan.sandhu6@gmail.com>
2025-05-20 20:49:26 -0600

    Fixed defpayorderAPI imports

--- frontend/src/Hooks/DefpayorderHooks/useDefPayOrder.jsx
+++ frontend/src/Hooks/DefpayorderHooks/useDefPayOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import defPayAPI from '@/APIs/defpayOrderAPI';
+import defPayAPI from '@/APIs/defpayorderAPI';
 import { useAuthContext } from '@/Hooks/AuthHooks/useAuthContext';
 
 export const useDefPayOrder = () => {
