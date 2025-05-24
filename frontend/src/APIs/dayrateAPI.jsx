0547c0a9d0deffea87c8dd1825c2d48bfd24c39f
Dilsan15 <dilshaan.sandhu6@gmail.com>
2025-05-23 21:27:26 -0600

    Update dayrateAPI.jsx

--- frontend/src/APIs/dayrateAPI.jsx
+++ frontend/src/APIs/dayrateAPI.jsx
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const dayrateAPI = axios.create({
-  baseURL: "http://localhost:5001/api/dayrates",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://petrol-pump-accounting.onrender.com/api/dayrates",
 });
 
 export default dayrateAPI;
